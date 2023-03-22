bbb666ff27d0ed69a838f06e9f2aec6a55f55901
Jaden Tagulinao <jadentagulinao@yahoo.com>
2023-03-22 00:49:21 -0700

    i'm goin to sleep bruh

--- JS/Pages/picklisting.js
+++ JS/Pages/picklisting.js
@@ -9,18 +9,16 @@ async function compileTeamSlotData() {
     var oN = curOrderNum++;
     await getTBAData("event/" + JSON.parse(localStorage.getItem("closestComp")).key + "/teams", oN);
     var teams = getOrder(oN);
-
-    document.getElementById("shopButton").innerHTML = (teams[0].team_number)
 }
 
 function toggleFiltersDiv() {
     var filtersDiv = document.getElementById("filtersDiv");
 
-    if (filtersDiv.style.bottom == "-320px") {
-        filtersDiv.style.bottom = "0px";
+    if (filtersDiv.style.bottom == "-600px") {
+        filtersDiv.style.bottom = "-280px";
         document.getElementById("filtersArrow").setAttribute("class", "flippedVertically");
     } else {
-        filtersDiv.style.bottom = "-320px";
+        filtersDiv.style.bottom = "-600px";
         document.getElementById("filtersArrow").setAttribute("class", "");
     }
 }
