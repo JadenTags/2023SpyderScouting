a3b7d3fe3febf8d18b710444507990077241c594
Jaden Tagulinao <jadentagulinao@yahoo.com>
2023-03-21 22:51:03 -0700

    testing on mobile

--- JS/Pages/picklisting.js
+++ JS/Pages/picklisting.js
@@ -3,18 +3,26 @@ var lockedDivs = {
     "shophDiv": "unlocked",
 };
 
-function compileTeamSlotData() {
-    
+async function compileTeamSlotData() {
+    await waitGlobalData();
+
+    var oN = curOrderNum++;
+    await getTBAData("event/" + JSON.parse(localStorage.getItem("closestComp")).key + "/teams", oN);
+    var teams = getOrder(oN);
+
+    document.getElementById("shopButton").innerHTML = (teams[0].team_number)
 }
 
 function toggleFiltersDiv() {
     var filtersDiv = document.getElementById("filtersDiv");
 
-    if (filtersDiv.style.bottom == "-300px") {
+    if (filtersDiv.style.bottom == "-320px") {
         filtersDiv.style.bottom = "0px";
         document.getElementById("filtersArrow").setAttribute("class", "flippedVertically");
     } else {
-        filtersDiv.style.bottom = "-300px";
+        filtersDiv.style.bottom = "-320px";
         document.getElementById("filtersArrow").setAttribute("class", "");
     }
 }
+
+compileTeamSlotData();
