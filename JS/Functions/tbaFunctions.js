c955fb499857a5400f997bb319ea17153a344b76
JadenTags <jadentagulinao@yahoo.com>
2023-03-23 13:56:29 -0700

    wd

--- JS/Functions/tbaFunctions.js
+++ JS/Functions/tbaFunctions.js
@@ -35,7 +35,6 @@ async function getClosestCompData(teamNum) {
     var orderNum = curOrderNum++;
     await getTBAData("team/frc" + teamNum + "/events", orderNum);
 
-    var curDate = testDate.getTime();
     var closestComp = getOrder(orderNum).reverse().sort((x, y) => new Date(y).getTime() - new Date(x).getTime()).filter(x => new Date(x.end_date).getTime() <= testDate.getTime())[0];
 
     if (!closestComp) {
@@ -43,6 +42,7 @@ async function getClosestCompData(teamNum) {
     }
 
     var difference = Math.floor((new Date(closestComp.start_date).getTime() - testDate.getTime()) / (1000 * 60 * 60 * 24));
+    console.log(closestComp.start_date,testDate.getTime(), (new Date(closestComp.start_date).getTime() - testDate.getTime()) )
     localStorage.setItem("closestComp", JSON.stringify(closestComp));
     localStorage.setItem("difference", difference);
 }
