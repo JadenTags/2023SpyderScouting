ccac4c8846747b346a73168e083174e341b5d164
JadenTags <jadentagulinao@yahoo.com>
2023-04-07 13:37:14 -0700

    change

--- JS/Pages/forms.js
+++ JS/Pages/forms.js
@@ -509,21 +509,7 @@ async function changeMatchAllianceButtons(selector) {
         changeNotif(selector + "MatchNotif", "")
         document.getElementById(selector + "MatchNumInput").style.border = "1px solid #c5c7c5";
     }
-
-
-    Object.keys(match.alliances).forEach(key => {
-        var alliance = match.alliances[key];
-        var counter = 1;
-
-        alliance.team_keys.forEach(team => {
-            let button = document.getElementById(selector + key[0].toUpperCase() + key.slice(1) + counter++ + "Button");
-            button.innerHTML = team.replace("frc", "");
-            button.setAttribute("onclick", button.getAttribute("onclick").replace("return;", ""));
-        });
-    });
     
-    showElement(selector + "MatchInnerDiv");
-
     var scoutedTeams = [];
     
     var orderNum = curOrderNum++;
@@ -534,21 +520,37 @@ async function changeMatchAllianceButtons(selector) {
         }
     });
 
-    Array.from(document.getElementsByClassName("inPersonAllianceButton")).forEach(button => {
-        if (scoutedTeams.includes(button.innerHTML)) {
-            button.innerHTML = "OK";
-            button.setAttribute("onclick", "return;" + button.getAttribute("onclick"));
+    Object.keys(match.alliances).forEach(key => {
+        var alliance = match.alliances[key];
+        var counter = 1;
 
-            if (button.getAttribute("class").includes("selectedButton")) {
-                togglePushButton(button.id);
+        alliance.team_keys.forEach(team => {
+            let button = document.getElementById(selector + key[0].toUpperCase() + key.slice(1) + counter++ + "Button");
+            
+            if (scoutedTeams.includes(button.innerHTML)) {
+                button.innerHTML = "OK";
+                button.setAttribute("onclick", "return;" + button.getAttribute("onclick"));
+
+                if (button.getAttribute("class").includes("selectedButton")) {
+                    togglePushButton(button.id);
+                }
+            } else {
+                button.innerHTML = team.replace("frc", "");
+                button.setAttribute("onclick", button.getAttribute("onclick").replace("return;", ""));
             }
-        }
+        });
+    });
+    
+    showElement(selector + "MatchInnerDiv");
+
+    Array.from(document.getElementsByClassName("inPersonAllianceButton")).forEach(button => {
+        
     });
 }
 
 async function cycleCheckChangeAllianceButtons() {
     while(true) {
-        await wait(4000);
+        await wait(10000);
 
         if (document.getElementById("inPersonBlue1Button").innerHTML != "One") {
             changeMatchAllianceButtons("inPerson")
