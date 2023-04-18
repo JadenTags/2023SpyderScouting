9ac6113b7023a4ebdd2089b8be5ce0c81e5277f7
Jaden Tagulinao <jadentagulinao@yahoo.com>
2023-04-18 00:46:13 -0700

    awd

--- JS/simuVars.js
+++ JS/simuVars.js
@@ -2,19 +2,21 @@ var testDate = new Date();
 //COMP: TYPE FIRST DATE OF COMP IN YEAR MONTH DAY FORM
 //NOT COMP: TYPE TEST DATE IN YEAR MONTH DAY FORM
 
-var sheetID = "1DGmIP0O3xx5Kt1TGLkmG93blUA9-LQ2YdLcH1gVTX28";
+var sheetID = "1wUO-tH25CUiifegzN42whYzGuugoqiyQJn0Gcp2xJAM";
 //COMP: USE THE ID THAT THE SHEETS USE
 //NOT COMP: LEAVE AS "1lydmTcdvkiqX3hrqJMzbgS5gNUQc97C-4zEcNFIbh_Y"
 //SD: 1lQFSP7u7PIwLp_dZhNAbfgSY1MM3Cgn2OC46d7ksz9g
 //BAYOU: 13upH6WD6_GKrwX00GqesO2RQtAKAhO-psJSHr2LJMm8
 //ROCKET CITY: 1X0r-ZXlazO1spQzebWH6v6bvmVZ5EZKVWi1ab9GRDHw
+//GALILEO: 1wUO-tH25CUiifegzN42whYzGuugoqiyQJn0Gcp2xJAM
 
-var roboPicsFolderId = "1gtz-9L2DWtX7kJwjKwjWrKLT1GgcOUpc";
+var roboPicsFolderId = "1jhOlr3TeDN3L2OeTQWc-OaNgLmMKinsP";
 //COMP: USE THE ID THAT THE SHEETS USE
 //NOT COMP: LEAVE AS "1pzk6rKsE_6CCRATpvFESwJ1E85CEB4kL"
 //SD: 1t42FXVXn0JYmXJIZvOM9yk-6XZHpaNVj
 //BAYOU: 15gtjdXvbqQQBifq9lvD6v4iYi67EShsd
 //ROCKET CITY: 1gtz-9L2DWtX7kJwjKwjWrKLT1GgcOUpc
+//GALILEO: 1jhOlr3TeDN3L2OeTQWc-OaNgLmMKinsP
 
 var stage = ["WARMUPS", "QUALS", "FINALS"][0];
 if (localStorage.getItem("displayData") == undefined) {
